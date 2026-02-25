f39031358a771abc5220fadb476bf67bb970d81c
privatetime <privatetime>
2026-02-25 16:55:39 +0800

    21

--- lib/common/window.dart
+++ lib/common/window.dart
@@ -17,7 +17,7 @@ class Window {
     if (system.isWindows) {
       protocol.register('clash');
       protocol.register('clashmeta');
-      protocol.register('flclash');
+      protocol.register('Fastfly');
     }
     await windowManager.ensureInitialized();
     // kDebugMode ? Size(680, 580) :
