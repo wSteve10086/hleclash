01aaac3fdbce9880150a083b7e3ee179dc8f72af
wSteve10086 <devidhartiz6@gmail.com>
2026-02-26 15:11:23 +0800

    修改window图标

--- setup.dart
+++ setup.dart
@@ -439,7 +439,7 @@ class BuildCommand extends Command {
     final mode = target == Target.android ? Mode.lib : Mode.core;
     final String out = argResults?['out'] ?? (target.same ? 'app' : 'core');
     final archName = argResults?['arch'];
-    final env = argResults?['env'] ?? 'pre';
+    final env = argResults?['env'] ?? 'release';
     final currentArches = arches
         .where((element) => element.name == archName)
         .toList();
